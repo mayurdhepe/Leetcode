f9ad281af83c2dbb2194ede6e571f13eeb0bbda4
Mayur Dhepe <mayurdhepe@gmail.com>
2022-01-31 23:15:33 -0500

    Time: 121 ms, Memory: 18.7 MB - LeetHub

--- inorder-successor-in-bst/inorder-successor-in-bst.py
+++ inorder-successor-in-bst/inorder-successor-in-bst.py
@@ -6,46 +6,15 @@
 #         self.right = None
 
 class Solution:
-    previous = None
-    inorder_successor = None
-    
     def inorderSuccessor(self, root: 'TreeNode', p: 'TreeNode') -> 'Optional[TreeNode]':
-        self.previous, self.inorder_successor = None, None
-        
-        if p.right:
-            leftmost = p.right
-            while leftmost.left:
-                leftmost = leftmost.left
+        successor = None
+        while root:
+            if p.val >= root.val:
+                root = root.right
+            else:
+                successor = root
+                root = root.left
                 
-            self.inorder_successor = leftmost
-        else:
-            self.inorderCase2(root, p)
+        return successor
             
-        return self.inorder_successor
-    
-    
-    def inorderCase2(self, node: 'TreeNode', p: 'TreeNode'):
-        if not node: return
-        
-        # recurse on the left side
-        self.inorderCase2(node.left, p)
-        
-        # Check if previous is the inorder predecessor of node
-        if self.previous == p and not self.inorder_successor:
-            self.inorder_successor = node
-            return
-        
-        # Keeping previous up-to-date for further recursions
-        self.previous = node
-        
-        # recurse on the right side
-        self.inorderCase2(node.right, p)
-    
-    
-    
-    
-    
-    
-    
-    
         
