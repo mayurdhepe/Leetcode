5fad1b9d640e6309577ef704cfede27c4b1b2416
Mayur Dhepe <mayurdhepe@gmail.com>
2022-02-22 21:50:42 -0500

    Time: 2631 ms, Memory: 102.1 MB - LeetHub

--- find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
+++ find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
@@ -1,32 +1,26 @@
+from collections import deque 
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        adjlist = self.buildGraph(n, edges)
+        adjList = [[] for _ in range(n)]
+        for u,v in edges:
+            adjList[u].append(v)
+            adjList[v].append(u)
+            
+        queue = deque([source])
+        visited = set([source])
         
-        stack = [source]
-        visited = set()
-        
-        while stack:
-            node = stack.pop()
+        while queue:
+            node = queue.popleft()
             
             if node == destination:
                 return True
             
-            if node in visited:
-                continue
-            
-            visited.add(node)
-            for neighbor in adjlist[node]:
-                stack.append(neighbor)
-        
+            for neighbor in adjList[node]:
+                if neighbor not in visited:
+                    visited.add(neighbor)
+                    queue.append(neighbor)
+                    
         return False
+                
             
-    
-    def buildGraph(self, n, edges):
-        adjlist = [[] for i in range(n)]
-        for a,b in edges:
-            adjlist[a].append(b)
-            adjlist[b].append(a)
-            
-        return adjlist
-        
-        
+                
