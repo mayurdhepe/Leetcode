469eb62197cc2fe6fa1cb92221c80c54a4e1cac6
Mayur Dhepe <mayurdhepe@gmail.com>
2022-02-24 00:04:25 -0500

    Time: 4679 ms, Memory: 124.4 MB - LeetHub

--- min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
+++ min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
@@ -6,25 +6,40 @@ class Solution:
         
         size = len(points)
         priorityQueue = []
-        uf = UnionFind(size)
+        visited = [False] * size
         
-        for i in range(size):
-            x1, y1 = points[i]
-            for j in range(i+1, size):
-                x2, y2 = points[j]
-                cost = abs(x1 - x2) + abs(y1 - y2)
-                edge = Edge(i, j, cost)
-                priorityQueue.append(edge)
-                
+        # Add all edges from points[0] vertexs
+        x1, y1 = points[0]
+        for j in range(1, size):
+            x2, y2 = points[j]
+            cost = abs(x1 - x2) + abs(y1 - y2)
+            edge = Edge(0, j, cost)
+            priorityQueue.append(edge)
+        
+        # heapify -- form min heap
         heapq.heapify(priorityQueue)
         
         result = 0
         count = size - 1  # MST has n-1 edges
+        visited[0] = True
+        
         while priorityQueue and count > 0:
             edge = heapq.heappop(priorityQueue)
-            if not uf.connected(edge.point1, edge.point2):
-                uf.union(edge.point1, edge.point2)
-                result += edge.weight
+            point1 = edge.point1
+            point2 = edge.point2
+            weight = edge.weight
+            
+            # if the point2 is not visited, then visit it
+            if not visited[point2]:
+                result += weight
+                visited[point2] = True
+                # add all its outgoing edges (to non-visited vertices) into the heap
+                for j in range(size):
+                    if not visited[j]:
+                        distance = abs(points[point2][0] - points[j][0]) + \
+                        abs(points[point2][1] - points[j][1])
+                        edge = Edge(point2, j , distance)
+                        heapq.heappush(priorityQueue, edge)
                 count -= 1
                 
         return result
@@ -38,31 +53,4 @@ class Edge:
     
     def __lt__(self, other):
         return self.weight < other.weight
-        
-class UnionFind:
-    def __init__(self, size):
-        self.root = [i for i in range(size)]
-        self.rank = [1] * size
-
-    def find(self, x):
-        if x == self.root[x]:
-            return x
-        self.root[x] = self.find(self.root[x])
-        return self.root[x]
-
-    def union(self, x, y):
-        rootX = self.find(x)
-        rootY = self.find(y)
-        if rootX != rootY:
-            if self.rank[rootX] > self.rank[rootY]:
-                self.root[rootY] = rootX
-            elif self.rank[rootX] < self.rank[rootY]:
-                self.root[rootX] = rootY
-            else:
-                self.root[rootY] = rootX
-                self.rank[rootX] += 1
-    
-    def connected(self, x, y):
-        return self.find(x) == self.find(y)
-                                
-        
+            
