c8a8b3dfccd0925488d1bbc977599980e029646d
Mayur Dhepe <mayurdhepe@gmail.com>
2022-03-03 22:53:10 -0500

    Time: 166 ms, Memory: 15.2 MB - LeetHub

--- replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.py
+++ replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.py
@@ -5,8 +5,11 @@ class Solution:
         maxVal = -1
        
         for i in range(n-1, -1, -1):
+            # Keep track of the 'would be' highest number
             tempMaxVal = max(maxVal, arr[i])
+            # Replace the index with the last known highest number
             arr[i] = maxVal
+            # Replace the last known highest number with the current highest number
             maxVal = tempMaxVal
             
         return arr
