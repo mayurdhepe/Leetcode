dd7627a4be64a3a6f82393ad09a5b7db81aa98fc
Mayur Dhepe <mayurdhepe@gmail.com>
2022-03-05 21:30:33 -0500

    Time: 469 ms, Memory: 14.3 MB - LeetHub

--- max-consecutive-ones-ii/max-consecutive-ones-ii.py
+++ max-consecutive-ones-ii/max-consecutive-ones-ii.py
@@ -4,17 +4,17 @@ class Solution:
         i, j = 0, 0
         numZeroes = 0
         maxOnes = 0
-        while j < len(nums):
-            if nums[j] == 0:
+        while j < len(nums):  # while our window is in bounds
+            if nums[j] == 0:   # add the right most element into our window
                 numZeroes += 1
                 
-            while numZeroes == 2:
+            while numZeroes == 2:   # if our window is invalid, contract our window
                 if nums[i] == 0:
                     numZeroes -= 1
                 i += 1
                 
-            maxOnes = max(maxOnes, j-i+1)
-            j += 1
+            maxOnes = max(maxOnes, j-i+1)   # update our longest sequence answer
+            j += 1    # expand our window
             
         return maxOnes
             
