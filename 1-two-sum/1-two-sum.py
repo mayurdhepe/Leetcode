adae04ff765204f565de4c2c09d13f066a632940
Mayur Dhepe <mayurdhepe@gmail.com>
2022-03-09 00:13:38 -0500

    Time: 86 ms (68.36%), Space: 15.2 MB (55.74%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,13 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        myDict = {}
-        n = len(nums)
-        for i in range(n):
-            comp = target - nums[i]
-            if (comp in myDict):
-                return [i, myDict[comp]]
-                
-            myDict[nums[i]] = i
-                
-                
+        hashMap = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in hashMap:
+                return [i, hashMap[complement]]
+            hashMap[num] = i
+            
         
