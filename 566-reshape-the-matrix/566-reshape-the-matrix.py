0018174668e59c82dc759f194e42003fd7950249
Mayur Dhepe <mayurdhepe@gmail.com>
2022-03-11 00:10:06 -0500

    Time: 84 ms (97.49%), Space: 14.8 MB (56.21%) - LeetHub

--- 566-reshape-the-matrix/566-reshape-the-matrix.py
+++ 566-reshape-the-matrix/566-reshape-the-matrix.py
@@ -8,15 +8,11 @@ class Solution:
             return mat
         
         result = [[0] * c for _ in range(r)]
-        result1 = [[0]*c]*r
-        print(result)
-        print(result1)
+       
         x, y = 0, 0
         for i in range(r):
             for j in range(c):
-                print(i,j)
                 result[i][j] = mat[x][y]
-                print(result[i])
                 y += 1
                 
                 if y == n:
