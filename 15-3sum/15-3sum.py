7558763c8a32f11cb30705c73164c1519ed333d7
Mayur Dhepe <mayurdhepe@gmail.com>
2022-03-26 23:10:24 -0400

    Time: 772 ms (88.04%), Space: 18 MB (80.80%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,26 +1,25 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
         nums.sort()
-        
-        # each # in input array as the possible first value
-        for i, a in enumerate(nums):
-            if (i > 0 and a == nums[i-1]):
+        res = []
+        for i, n in enumerate(nums):
+            if i > 0 and n == nums[i-1]:
                 continue
-                
-            # solve 2 sum for L + R by 2 pointer method  
+            
             l, r = i+1, len(nums)-1
             while l<r:
-                threeSum = a + nums[l] + nums[r]
+                threeSum = n + nums[l] + nums[r]
                 if threeSum > 0:
                     r -= 1
                 elif threeSum < 0:
                     l += 1
                 else:
-                    res.append([a, nums[l], nums[r]])
+                    res.append([n, nums[l], nums[r]])
                     l += 1
                     # [-2, -2, -2, 0, 0, 2, 2]
                     while nums[l] == nums[l-1] and l < r:
                         l += 1
         
         return res
+            
+        
