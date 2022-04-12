1c776da5d9596df18a586dda552bbe7e3e69718a
Mayur Dhepe <mayurdhepe@gmail.com>
2022-04-11 20:17:55 -0400

    Time: 32 ms (96.45%), Space: 14 MB (33.23%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -4,20 +4,15 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         n = len(matrix)
-        
-        # transponse the matrix
+        # transponse the matrix across diagonal
         for i in range(n):
             for j in range(i+1, n):
-                matrix[j][i],  matrix[i][j] =  matrix[i][j],  matrix[j][i]
-        
-        # flip the first and last columns
-        for i in range(n):
-            for j in range(n // 2):
-                matrix[i][j],  matrix[i][n-1-j] =  matrix[i][n-1-j],  matrix[i][j]
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j] 
                 
         
+        for i in range(n):
+            for j in range(n//2):
+                matrix[i][j], matrix[i][n-1-j] = matrix[i][n-1-j], matrix[i][j]
+                
         return matrix
                 
-        
-        
-        
